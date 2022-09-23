cfe39209dae5ae98a60e8466bc6667a869b2dc77
Vladimir <motiw@icloud.com>
2022-09-23 18:02:55 +0600

    Add Maraphone App

---
+++ Marathon/Targets/Marathon/Sources/AppDelegate.swift
@@ -0,0 +1,25 @@
+import UIKit
+import MarathonKit
+import MarathonUI
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .red
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+        MarathonKit.hello()
+        MarathonUI.hello()
+
+        return true
+    }
+
+}
---
+++ Marathon/Targets/MarathonUI/Tests/MarathonUITests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class MarathonUITests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("MarathonUI", "MarathonUI")
+    }
+}
