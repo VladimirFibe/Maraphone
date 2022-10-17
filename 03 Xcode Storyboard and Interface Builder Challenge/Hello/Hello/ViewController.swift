952d8cd177d0a0419c29d41e56243d84054ffa1d
Vladimir <motiw@icloud.com>
2022-10-17 21:48:14 +0600

    Dicee

---
+++ 03 Xcode Storyboard and Interface Builder Challenge/Hello/Hello/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by Vladimir Fibe on 13.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
