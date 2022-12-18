4767a3bfabd59bf0a9884b1c44f7b4e06dc658b6
Vladimir Fibe <motiw@icloud.com>
2022-12-18 15:18:44 +0600

    Chess

---
+++ 03 Xcode Storyboard and Interface Builder Challenge/Chess/Chess/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chess
+//
+//  Created by Vladimir Fibe on 12/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
