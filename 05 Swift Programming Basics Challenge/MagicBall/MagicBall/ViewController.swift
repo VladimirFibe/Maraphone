038483a4b86d35c5ae0c5f637cee7846129e6b61
fibeapp <motiw@me.com>
2023-01-18 00:07:09 +0600

    add music

--- 05 Swift Programming Basics Challenge/MagicBall/MagicBall/ViewController.swift
+++ 05 Swift Programming Basics Challenge/MagicBall/MagicBall/ViewController.swift
@@ -1,6 +1,8 @@
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    var player: AVAudioPlayer?
 
     let answers = [#imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: "ball5")]
     @IBOutlet weak var titleLabel: UILabel!
@@ -12,6 +14,8 @@ class ViewController: UIViewController {
         imageView.image = answers.randomElement()
         view.backgroundColor = .systemBlue
         askButton.backgroundColor = .white
+        player?.numberOfLoops = -1
+        playSound()
     }
 
 
@@ -23,6 +27,20 @@ class ViewController: UIViewController {
       if motion == .motionShake {
           imageView.image = answers.randomElement()
       }
+    }
+    
+    func playSound() {
+        guard let path = Bundle.main.path(forResource: "background", ofType:"mp3") else {
+            return }
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
     }
 }
 
