b7e8dce8b14e099690fdf4a379365e340e5c1a80
Color Lines <color.lines@icloud.com>
2023-01-24 21:28:41 +0600

    new app

---
+++ 02 Xcode Storyboards and Interface Builder/ImRich/ImRich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImRich
+//
+//  Created by Color Lines on 1/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
